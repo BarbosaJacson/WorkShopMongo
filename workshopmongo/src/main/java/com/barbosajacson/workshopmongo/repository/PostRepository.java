50aced99026c88dd611c233909e6f5bf1c0f3718
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-01-12 16:08:36 -0300

    feat: complete workshop-mongo project with all advanced search criteria

--- workshopmongo/src/main/java/com/barbosajacson/workshopmongo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/barbosajacson/workshopmongo/repository/PostRepository.java
@@ -4,7 +4,7 @@ import com.barbosajacson.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -13,6 +13,11 @@ public interface PostRepository extends MongoRepository<Post,String> {
     @Query("{'title':{$regex: ?0, $options: 'i', }}")
     List<Post> searchTitle(String text);
     List<Post>findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , " +
+            "{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, " +
+            "{ 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 
 }
